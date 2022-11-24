b582634f982ab6870594da8a1cc21959975843e3
elise <elise.verdier@etu.emse.fr>
2022-11-24 16:29:20 +0100

    CleverCloud

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './station.module';
 
 async function bootstrap() {
-<<<<<<< HEAD
+
   const port = process.env.PORT; //3000 for postman
-=======
-  const port = process.env.PORT; //3000
->>>>>>> 999adf3522e3c93e9a6fcfc9826491405cb17820
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
